973b89df8cd6f21a275c092bf1891f1a5bf6cd57
nico2p <nico2p@protonmail.com>
2018-01-15 16:03:34 +0100

    User and Security Firewall base
    Mise en place des utilisateurs, rôles et firewall

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -14,10 +14,12 @@ use AppBundle\Form\ProjectEditType;
 use AppBundle\Form\ProjectType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ProjectController extends Controller
 {
@@ -44,6 +46,7 @@ class ProjectController extends Controller
     /**
      * @Route("/projects/add")
      * @Method({"GET", "POST"})
+     *@Security("has_role('ROLE_AUTEUR')")
      */
     public function projectAdd(Request $request)
     {
---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nico
+ * Date: 15/01/2018
+ * Time: 15:05
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+        {
+          return $this->redirectToRoute('homepage');
+        }
+
+        $authetivationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $authetivationUtils->getLastUsername(),
+            'error' => $authetivationUtils->getLastAuthenticationError(),
+        ));
+    }
+}
